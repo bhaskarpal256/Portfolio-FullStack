a735bc1d293a0ed2e354e10f675f7120384289eb
harmeetpal258 <123814187+harmeetpal258@users.noreply.github.com>
2026-06-25 22:11:21 +0530

    Making logging work in phone and safari

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,5 +3,14 @@ import { refreshAccessToken } from "./auth.service";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
