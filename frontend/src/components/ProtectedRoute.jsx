bc3da67924336819c3a716c10aedd83f26199500
harmeetpal258 <123814187+harmeetpal258@users.noreply.github.com>
2026-06-30 20:37:24 +0530

    Changed Loading Screen in Protected Route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,10 +1,16 @@
 import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../context/AuthContext";
+import LoadingScreen from "./ui/LoadingScreen";
 
 const ProtectedRoute = () => {
     const { user, loading } = useAuth();
 
-    if (loading) return <div>Loading...</div>
+    if (loading) return (
+    <LoadingScreen
+      title="LOADING"
+      subtitle="PLEASE WAIT..."
+    />
+  );
 
     if (!user) return <Navigate to="/login" replace /> //what replace does is when you go to /admin and if you're not logged in you are re-directed to /login but without replace if you click the back button on your browser when you're on the /login page is that you're sent back to /admin that again sends you to /login , so it becomes a unintentional back-button loop
 
