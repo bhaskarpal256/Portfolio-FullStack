6280811e772b1d33f18dc06cccdafe9f14147572
harmeetpal258 <123814187+harmeetpal258@users.noreply.github.com>
2026-06-30 19:54:52 +0530

    Fixing Login Authentication

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,22 @@
 import axios from "axios";
-import { refreshAccessToken } from "./auth.service";
+import { refreshAccessToken } from "./auth.service.js";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  
 });
+console.log(import.meta.env.VITE_API_URL);
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+
+    console.log("REQUEST:", config.url);
+  console.log("TOKEN:", token);
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
