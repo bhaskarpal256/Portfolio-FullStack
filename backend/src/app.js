db92be1dd8288e4f1bcb0aa5c9c7c3c8b4dd0fe8
harmeetpal258 <123814187+harmeetpal258@users.noreply.github.com>
2026-06-30 20:13:01 +0530

    Set CORS_ORIGIN IN app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ import { errorHandler } from "../middlewares/error.middleware.js";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
